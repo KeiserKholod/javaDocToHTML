a653f5503cdc13a0228eff17946ca8d39103f874
Dimitry <Kholod.ural@gmail.com>
2021-01-20 13:54:42 +0500

    added argparser and main

---
+++ javaDocToHTML/__main__.py
@@ -0,0 +1,29 @@
+import argparse
+from pathlib import Path
+
+
+def get_files_from_dir(dir_name: str, files):
+    for file in Path(dir_name).iterdir():
+        if file.is_dir():
+            get_files_from_dir(str(file), files)
+        if file.name.endswith('.java'):
+            pass
+    return files
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='javaDocToHTML')
+    parser.add_argument('files', type=str, nargs='+', default=[],
+                        help='Path to files or dir\'s with files')
+    return parser
+
+
+if __name__ == '__main__':
+    args = get_parser().parse_args()
+    files_or_dirs = args.files
+    for directory in files_or_dirs:
+        if Path(directory).exists():
+            files = get_files_from_dir(directory, [])
+            # convert
+        else:
+            print(f'directory {directory} doesnt exist.')
